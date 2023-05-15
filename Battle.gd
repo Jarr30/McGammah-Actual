5bb33097cf3dc56757c9c2c8bd1006a91a64ff51
Jarr30 <116113343+Jarr30@users.noreply.github.com>
2023-05-15 14:11:48 -0500

    So far game

---
+++ Battle.gd
@@ -0,0 +1,68 @@
+extends Control
+
+signal textbox_closed
+
+export(Resource) var enemy = null
+
+var current_player_health = 0
+var current_enemy_health = 0
+
+func _ready():
+	set_health($EnemyContainer/ProgressBar, enemy.health, enemy.health)
+	set_health($PlayerPanel/PlayerData/ProgressBar, State.current_health, State.max_health)
+	$EnemyContainer/Enemy.texture = enemy.texture
+	
+	current_player_health = State.current_health
+	current_enemy_health = enemy.health
+	
+	$TextBox.hide()
+	$ActionsPanel.hide()
+	
+	display_text("%s challenged you to a fight!" % enemy.name.to_upper())
+	yield(self, "textbox_closed")
+	$ActionsPanel.show()
+
+func set_health(progress_bar, health, max_health):
+	progress_bar.value = health
+	progress_bar.max_value = max_health
+	progress_bar.get_node("Label").text = "HP:%d/%d" % [health, max_health]
+
+func _input(event):
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(BUTTON_LEFT)) and $TextBox.visible:
+		$TextBox.hide()
+		emit_signal("textbox_closed")
+
+func display_text(text):
+	$TextBox.show()
+	$TextBox/Label.text = text
+	
+func enemy_turn():
+	display_text("%s played 3 hours of shreksaphone!" % enemy.name.to_upper())
+	yield(self, "textbox_closed")
+	
+	current_player_health = max(0, current_player_health - enemy.damage)
+	set_health($PlayerPanel/PlayerData/ProgressBar, current_player_health, State.max_health)
+	
+	$AnimationPlayer.play("shake")
+	yield($AnimationPlayer, "animation_finished")
+	
+	display_text("%s dealt %d damage!" % [enemy.name.to_upper(), enemy.damage])
+	yield(self, "textbox_closed")
+
+func _on_Run_pressed() -> void:
+	display_text("You ran like a coward!")
+	yield(self, "textbox_closed")
+	yield(get_tree().create_timer(0.25), "timeout")
+	get_tree().quit()
+
+func _on_Attack_pressed() -> void:
+	display_text("You comment on his bad practice! You deal %d damage!" % State.damage )
+	yield(self, "textbox_closed")
+	
+	current_enemy_health = max(0, current_enemy_health - State.damage)
+	set_health($EnemyContainer/ProgressBar, current_enemy_health, enemy.health)
+	
+	$AnimationPlayer.play("enemy_damaged")
+	yield($AnimationPlayer, "animation_finished")
+
+	enemy_turn()
